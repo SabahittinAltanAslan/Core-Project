fadf1cce0870fee2cf8f4d218117a06dc8ecf647
Sabahittin Altan Aslan <122750012+SabahittinAltanAslan@users.noreply.github.com>
2023-09-28 23:58:02 +0300

    Core-Project
    C#, Asp.Net Core MVC , SQL Server, Linq, Html, CSS

---
+++ ProjeCore/ProjeCore/Controllers/DefaultController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjeCore.Models;
+
+namespace ProjeCore.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var degerler = c.Birims.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public IActionResult YeniBirim()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult YeniBirim(Birim b)
+        {
+            c.Birims.Add(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult BirimSil(int id)
+        {
+            var birim = c.Birims.Find(id);
+            c.Birims.Remove(birim);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult    BirimGetir(int id)
+        {
+            var depart = c.Birims.Find(id);
+            return View(depart);
+        }
+        public IActionResult BirimGuncelle(Birim b)
+        {
+            var dep = c.Birims.Find(b.BirimID);
+            dep.BirimAd = b.BirimAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult BirimDetay(int id)
+        {
+            var degerler=c.Personels.Where(x=>x.BirimID==id).ToList();
+            var brmad =c.Birims.Where(x => x.BirimID == id).Select(y=>y.BirimAd).FirstOrDefault();
+            ViewBag.brm=brmad;
+            return View(degerler);
+        }
+    }
+}
---
+++ ProjeCore/ProjeCore/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjeCore.Models
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LEGEN\\MSSQLSERVER01;database=BirimDb;integrated security= true; Trust Server Certificate=true;");
+        }
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+    }
+}
